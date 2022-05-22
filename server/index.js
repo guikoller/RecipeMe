878019a17cf42ebb1995318c3c91f38638a570c2
guikoller <g_koller@hotmail.com>
2022-05-22 11:35:31 -0300

    arrumei o cabeçalho e jwt gerando o token

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const jsonParser = bodyParser.json()
 const port = 8081
+const express = require('express')
+const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
-
 const Authenticator = require('./authenticator')
 
+const jsonParser = bodyParser.json()
+const app = express()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
 const JWTPassword = 'laudelindo'
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 
 app.get("/ping",(req,res) => {
     res.send("pong")
@@ -21,9 +23,15 @@ app.post("/auth", jsonParser,(req,res) => {
     let check = new Authenticator(email, senha)
 
     if(check.auth()){
-        jwt.sign({email: email})
-        res.status(200)
-        res.send("TOKEN")
+        jwt.sign({email: email},JWTPassword, {expiresIn: '48h'}, (err, token) => {
+            if (err) {
+                res.status(400)
+                res.send("ERROR: " + err )
+            } else {
+                res.status(200)
+                res.send({'jwt': token})
+            }
+        })
     }else{
         res.status(401)
         res.send("ERROR")
