5f993c38c68b13707cc37857ad94dfb6dd0970d9
guikoller <g_koller@hotmail.com>
2022-05-22 15:53:21 -0300

    componente de login que não funciona ainda

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <LoginComponent> </LoginComponent>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginComponent from './components/Login.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginComponent
   }
 }
 </script>
---
+++ client/src/components/Login.vue
@@ -0,0 +1,158 @@
+<template>
+    <div class="login-page">
+        <div class="form">
+            <form @submit.prevent="submit" class="login-form">
+                <input v-model="data.email" type="text" placeholder="Usuário" required/>
+                <input v-model="data.senha" type="password" placeholder="Senha" required/>
+                <button type="submit">login</button>
+                <p class="message">Não é Registrado? <a href="#">Criar uma conta</a></p>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+
+export default {
+    name: 'LoginComponent',
+    setup(){
+        const data = reactive({
+            email: 'teste',
+            senha: 'teste'
+        });
+    
+        //const router = useRouter();
+
+        const submit = async () => {
+            const url = 'http://localhost:8081/auth'
+
+
+
+            await fetch(url, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                mode: 'cors',
+                cache: 'default',
+                body: JSON.stringify(data)
+            })
+
+            // await router.push('/');
+        }
+
+        return {
+            data,
+            submit
+        }
+    }
+}
+</script>
+
+<style scoped>
+.login-page {
+    width: 360px;
+    padding: 8% 0 0;
+    margin: auto;
+}
+
+.form {
+    position: relative;
+    z-index: 1;
+    background: #FFFFFF;
+    max-width: 360px;
+    margin: 0 auto 100px;
+    padding: 45px;
+    text-align: center;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.05);
+}
+
+.form input {
+    font-family: "Roboto", sans-serif;
+    outline: 0;
+    background: #f2f2f2;
+    width: 100%;
+    border: 0;
+    margin: 0 0 15px;
+    padding: 15px;
+    box-sizing: border-box;
+    font-size: 14px;
+}
+
+.form button {
+    font-family: "Roboto", sans-serif;
+    text-transform: uppercase;
+    outline: 0;
+    background: #138217;
+    width: 100%;
+    border: 0;
+    padding: 15px;
+    color: #FFFFFF;
+    font-size: 14px;
+    -webkit-transition: all 0.3 ease;
+    transition: all 0.3 ease;
+    cursor: pointer;
+}
+
+.form button:hover,
+.form button:active,
+.form button:focus {
+    background: #43A047;
+}
+
+.form .message {
+    margin: 15px 0 0;
+    color: #b3b3b3;
+    font-size: 12px;
+}
+
+.form .message a {
+    color: #4CAF50;
+    text-decoration: none;
+}
+
+.form .register-form {
+    display: none;
+}
+
+.container {
+    position: relative;
+    z-index: 1;
+    max-width: 300px;
+    margin: 0 auto;
+}
+
+.container:before,
+.container:after {
+    content: "";
+    display: block;
+    clear: both;
+}
+
+.container .info {
+    margin: 50px auto;
+    text-align: center;
+}
+
+.container .info h1 {
+    margin: 0 0 15px;
+    padding: 0;
+    font-size: 36px;
+    font-weight: 300;
+    color: #1a1a1a;
+}
+
+.container .info span {
+    color: #4d4d4d;
+    font-size: 12px;
+}
+
+.container .info span a {
+    color: #000000;
+    text-decoration: none;
+}
+
+.container .info span .fa {
+    color: #EF3B3A;
+}
+
+</style>
