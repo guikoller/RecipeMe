b2f2c6af533ce16b1898d966bd680c41689e6d68
guikoller <g_koller@hotmail.com>
2022-05-21 21:18:30 -0300

    começo da autenticação por token

--- server/index.js
+++ server/index.js
@@ -1,7 +1,34 @@
 const express = require('express')
 const app = express()
-
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
 const port = 8081
+const jwt = require('jsonwebtoken')
+
+const Authenticator = require('./authenticator')
+
+const JWTPassword = 'laudelindo'
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.get("/ping",(req,res) => {
+    res.send("pong")
+})
+
+app.post("/auth", jsonParser,(req,res) => {
+    let email = req.body.email
+    let senha = req.body.senha
+
+    let check = new Authenticator(email, senha)
+
+    if(check.auth()){
+        jwt.sign({email: email})
+        res.status(200)
+        res.send("TOKEN")
+    }else{
+        res.status(401)
+        res.send("ERROR")
+    }
+})
 
 app.listen(port, () => {
     console.log("Server running at the port " + port)
